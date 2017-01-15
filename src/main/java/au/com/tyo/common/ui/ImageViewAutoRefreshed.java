9c2263c23b02d5052f77ff8689beb03053c97cbf
tyodev <deve@tyo.com.au>
2017-01-15 15:23:12 +1000

    minor updates

---
+++ src/main/java/au/com/tyo/common/ui/ImageViewAutoRefreshed.java
@@ -0,0 +1,55 @@
+package au.com.tyo.common.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by "Eric Tang (dev@tyo.com.au)" on 14/1/17.
+ */
+
+public class ImageViewAutoRefreshed extends ImageView {
+	
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public ImageViewAutoRefreshed(Context context) {
+		super(context);
+		
+	}
+	
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public ImageViewAutoRefreshed(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+	}
+	/**
+	 * @param context
+	 * @param attrs
+	 * @param defStyleAttr
+	 */
+	public ImageViewAutoRefreshed(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		
+	}	
+
+	/**
+	 * @param context
+	 * @param attrs
+	 * @param defStyleAttr
+	 * @param defStyleRes
+	 */
+	public ImageViewAutoRefreshed(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+		
+	}
+
+
+
+
+
+}
