9cd01afb8f00102f8516c3fed7265d163de147bb
tyodev <deve@tyo.com.au>
2017-05-11 14:37:39 +1000

    add a few widgets

---
+++ src/main/java/au/com/tyo/common/ui/BoxCardView.java
@@ -0,0 +1,39 @@
+package au.com.tyo.common.ui;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by monfee on 14/3/17.
+ */
+
+public class BoxCardView extends CardView implements ImageViewHolder {
+
+    private ImageView imageView;
+
+    public BoxCardView(Context context) {
+        super(context);
+    }
+
+    public BoxCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BoxCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        imageView = (ImageView) findViewById(R.id.box_image_view);
+    }
+}
---
+++ src/main/java/au/com/tyo/common/ui/CardBox.java
@@ -0,0 +1,49 @@
+package au.com.tyo.common.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by monfee on 22/3/17.
+ */
+
+public class CardBox extends PreviewBox {
+
+    private int index;
+
+    public CardBox(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    public CardBox(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    public CardBox(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init(context);
+    }
+
+    private void init(Context context) {
+
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+
+}
